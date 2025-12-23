da8bead40037c51c0dc60440add01296da2b34c5
codeverseinnovation <142432342+sharjeel-ahmed17@users.noreply.github.com>
2025-12-23 16:22:53 +0500

    added

--- backend/src/api/deps.py
+++ backend/src/api/deps.py
@@ -2,10 +2,9 @@ from datetime import datetime, timedelta
 from uuid import UUID
 from typing import Generator
 
-import jwt
+from jose import jwt, JWTError
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jwt.exceptions import PyJWTError
 import bcrypt
 from passlib.context import CryptContext
 from sqlmodel import Session
